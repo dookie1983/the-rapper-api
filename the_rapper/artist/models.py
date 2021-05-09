f4c00278b3a04e81fdd430fd8c36151e2745ed86
KoDookie <nepter2526@gmail.com>
2021-05-09 16:48:24 +0700

    rename table artist_artist to artist

--- the_rapper/artist/models.py
+++ the_rapper/artist/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-# Create your models here.
-# - aka
-# - name
-# - age
-
 class Artist(models.Model):
     aka = models.CharField(max_length=100)
     name = models.CharField(max_length=255)
     age = models.IntegerField()
 
-class Meta:
-    db_table = 'artist'
+    class Meta:
+        db_table = 'artist'
